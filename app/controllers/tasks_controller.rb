327534658b1271d08d14012a6541b22faf1cd36f
Melanie <m1114m1127@gmail.com>
2024-05-26 14:21:33 +0800

    finished update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.order(:id)
   end
 
   def new
@@ -14,6 +14,20 @@ class TasksController < ApplicationController
       redirect_to tasks_path, notice: "Successfully added task!"
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @task = Task.find_by(id: params[:id])
+  end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "Updated Successfully!"
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
