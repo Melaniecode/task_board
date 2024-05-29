51b31584cf4c69d71e42396a4a25d5f10804d8ea
Melanie <m1114m1127@gmail.com>
2024-05-29 18:31:10 +0800

    rupocop修改

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Controller responsible for managing tasks in the application.
 class TasksController < ApplicationController
   helper_method :task
 
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# provides common functionality and configurations for all models.
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
