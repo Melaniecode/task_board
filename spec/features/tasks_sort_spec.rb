b50169d7b73aa5507f823cf44c36820f01ff7706
Melanie <m1114m1127@gmail.com>
2024-06-03 23:21:13 +0800

    rewrite tasks_sort_spec and controller

--- spec/features/tasks_sort_spec.rb
+++ spec/features/tasks_sort_spec.rb
@@ -21,30 +21,38 @@ RSpec.describe 'Task Sort Management' do
     end
 
     context 'when order by id desc' do
+      before { order_by('id', 'desc') }
+
       it { expect(find(first_tr)).to have_text('Task 2') }
       it { expect(find(second_tr)).to have_text('Task 1') }
     end
 
     context 'when order by id asc' do
+      before { order_by('id', 'asc') }
+
       it { expect(find(first_tr)).to have_text('Task 1') }
       it { expect(find(second_tr)).to have_text('Task 2') }
     end
 
     context 'when order by created_at desc' do
-      it { expect(find(second_tr)).to have_text('Task 2') }
+      before { order_by('created_at', 'desc') }
+
+      it { expect(find(first_tr)).to have_text('Task 2') }
       it { expect(find(second_tr)).to have_text('Task 1') }
     end
 
     context 'when order by created_at asc' do
+      before { order_by('created_at', 'asc') }
+
       it { expect(find(first_tr)).to have_text('Task 1') }
       it { expect(find(second_tr)).to have_text('Task 2') }
     end
 
     def order_by(column, row)
       within('#sort') do
-        select(I18n.t("order_options.#{column}_#{row}"), from: 'order_by')
+        select(I18n.t("order_options.#{column}_#{row}"), from: 'sort_by')
       end
-      click_on I18n.t('.confirm')
+      click_on I18n.t('tasks.index.confirm')
     end
   end
 end
