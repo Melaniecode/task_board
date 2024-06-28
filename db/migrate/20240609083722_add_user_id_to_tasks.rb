6325f0468ec57d67d97761d2ac71c0402df467ee
Melanie <m1114m1127@gmail.com>
2024-06-28 18:17:11 +0800

    Fix: 解決null:false會導致migrate失敗問題

--- db/migrate/20240609083722_add_user_id_to_tasks.rb
+++ db/migrate/20240609083722_add_user_id_to_tasks.rb
@@ -1,5 +1,7 @@
 class AddUserIdToTasks < ActiveRecord::Migration[7.1]
   def change
-    add_reference :tasks, :user, null: false
+    add_reference :tasks, :user
+    Task.where(user_id: nil).update_all(user_id: User.first.id)
+    change_column_null :tasks, :user_id, false
   end
 end
